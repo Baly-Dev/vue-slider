02557295d800644c07cede23e1a3d0bcbcbf488f
baly <kevin@baldev.it>
2022-07-25 10:46:34 +0200

    Project done

---
+++ js/main.js
@@ -0,0 +1,66 @@
+let app = new Vue({
+    el:'#app',
+    data: {
+        // counter
+        c: 0,
+
+        // images objects
+        images: [
+            {
+                url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+                title: 'Svezia',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+            },
+        
+            {
+                url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+                title: 'Perù',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+            },
+        
+            {
+                url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+                title: 'Chile',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+            },
+            {
+                url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+                title: 'Argentina',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+            },
+            {
+                url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+                title: 'Colombia',
+                description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+            },
+        ]
+    },
+    methods: {
+        // method to visualize the current carousel object
+        currentObj(i) {
+            if (i == this.c) {
+                return 'd-flex';
+            } else {
+                return 'd-none';
+            }
+        },
+        
+        // method to go forward
+        nextObj() {
+            if (this.c == this.images.length - 1) {
+                this.c = 0;
+            } else {
+                this.c++;
+            }
+        },
+
+        // method to go backward
+        prevObj() {
+            if (this.c == 0) {
+                this.c = this.images.length - 1
+            } else {
+                this.c--;
+            }
+        }
+    }
+});
